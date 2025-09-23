ecdeebf32d30c26f6de49f1acac4ed87a69e6232
KimTerrence <quineskmterrence@gmail.com>
2025-09-23 18:32:10 +0800

    added routes for new pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,9 @@ import Download from "./pages/Download";
 import Pest from "./pages/Pest";
 import AdminLayout from "./pages/admin/AdminLayout";
 import AdminDashboard from "./pages/admin/Dashboard";
+import User from "./pages/admin/User";
+import Data from "./pages/admin/Data";
+import AdminPest from "./pages/admin/AdminPest";
 
 function PrivateRoute() {
   const token = localStorage.getItem("authToken");
@@ -26,6 +29,10 @@ function App() {
       <Route element={<PrivateRoute />}>
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<AdminDashboard />} /> 
+
+          <Route path="/admin/users" element={<User />} /> 
+          <Route path="/admin/pest" element={<AdminPest />} /> 
+          <Route path="/admin/data" element={<Data />} />       
         </Route>
       </Route>
     </Routes>
